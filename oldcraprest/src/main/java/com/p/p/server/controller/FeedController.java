e67aac9fd9437cd69bd2eb982748e4465e18ae98
mihail.nenov <mihail.nenov@ontotext.com>
2014-10-28 20:24:35 +0200

    More work.

---
+++ oldcraprest/src/main/java/com/p/p/server/controller/FeedController.java
@@ -0,0 +1,30 @@
+package com.p.p.server.controller;
+
+import com.p.p.server.model.bean.Posting;
+import com.p.p.server.util.DBUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class FeedController {
+
+	@Autowired
+	DBUtils dbUtils;
+
+	@RequestMapping(value = { "/feed" }, method = RequestMethod.GET)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	public List<Posting> feed(@RequestParam(required = false) Date date, @RequestParam(required = false,
+	  defaultValue = "128") Integer limit) {
+
+		date = date != null ? date : Calendar.getInstance().getTime();
+
+		return dbUtils.getPostingsBefore(date, limit);
+	}
+}
